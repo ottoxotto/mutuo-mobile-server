2da66a345379a06a2f084477c2b8019217b4f486
ottoxotto <alessandro.ottaviani92@gmail.com>
2022-10-10 16:47:39 +0200

    1st committ

---
+++ index.py
@@ -0,0 +1,69 @@
+from flask import Flask, request
+from flask_restful import Api
+import json
+
+from MyFunctions import CalcolaMutuoAPI
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route("/")
+def home():
+    return 'Home Page Route'
+
+@app.route("/outMutuo", methods = ["POST"])
+def returnDataDetails():
+
+    # global response
+
+    request_data = request.data
+    request_data = json.loads(request_data.decode("utf-8"))
+    OutputsMutuo, OutputsAnnuoMutuo, OutputAvgMutuo, OutputOverviewMutuo = CalcolaMutuoAPI(request_data)
+    dummy=1
+    ReturnData = OutputsMutuo.to_json()
+
+    return ReturnData
+
+@app.route("/outMutuoAvg", methods = ["POST"])
+def returnDataAnno():
+
+    # global response
+
+    request_data = request.data
+    request_data = json.loads(request_data.decode("utf-8"))
+    OutputsMutuo, OutputsAnnuoMutuo, OutputAvgMutuo, OutputOverviewMutuo = CalcolaMutuoAPI(request_data)
+    dummy=1
+    ReturnData = OutputsAnnuoMutuo.to_json()
+
+    return ReturnData
+
+@app.route("/outMutuoAvgTot", methods = ["POST"])
+def returnDataAvg():
+
+    # global response
+
+    request_data = request.data
+    request_data = json.loads(request_data.decode("utf-8"))
+    OutputsMutuo, OutputsAnnuoMutuo, OutputAvgMutuo, OutputOverviewMutuo = CalcolaMutuoAPI(request_data)
+    dummy=1
+    ReturnData = OutputAvgMutuo.to_json()
+
+    return ReturnData
+
+@app.route("/outMutuoOverview", methods = ["POST"])
+def returnDataOv():
+
+    # global response
+
+    request_data = request.data
+    request_data = json.loads(request_data.decode("utf-8"))
+    OutputsMutuo, OutputsAnnuoMutuo, OutputAvgMutuo, OutputOverviewMutuo = CalcolaMutuoAPI(request_data)
+    dummy=1
+    ReturnData = OutputOverviewMutuo.to_json()
+
+    return ReturnData
+
+
+
+# if __name__ == "__main__":
+#     app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
